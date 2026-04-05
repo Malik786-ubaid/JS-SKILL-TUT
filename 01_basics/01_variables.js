1c9c2aa54503feee9019bd07593af45bde209505
Malik Ubaid Awan <malikubaidawan14@gmail.com>
2026-04-05 04:42:48 +0000

    add files and delete

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,15 +1,15 @@
 const accountId = 144553
-let accountEmail = "Attiq@google.com"
+let accountEmail = "hitesh@google.com"
 var accountPassword = "12345"
-accountCity = "Malalwal"
+accountCity = "Jaipur"
 let accountState;
 
 // accountId = 2 // not allowed
 
 
-accountEmail = "malik@.com"
+accountEmail = "hc@hc.com"
 accountPassword = "21212121"
-accountCity = "ISB"
+accountCity = "Bengaluru"
 
 console.log(accountId);
 
@@ -19,4 +19,4 @@ because of issue in block scope and functional scope
 */
 
 
-// console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
